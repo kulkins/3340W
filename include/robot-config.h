7fb3821f7d52c5d59d46ae0a7041163cac5b85f8
Tidris34 <tidris1453@gmail.com>
2025-04-03 20:18:20 -0700

    c

--- include/robot-config.h
+++ include/robot-config.h
@@ -4,7 +4,7 @@ extern brain Brain;
 
 //To set up a motor called LeftFront here, you'd use
 //extern motor LeftFront;
-
+// balls
 //Add your devices below, and don't forget to do the same in robot-config.cpp:
 
 extern motor intake1;
